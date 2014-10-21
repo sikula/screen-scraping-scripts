f7c4e80d27e062c324df9ea4b0d1ad1997eaf2c7
sikula <rocco56@gmail.com>
2014-10-21 17:34:16 -0400

    added two script to the repository

---
+++ openmap_scrape.js
@@ -0,0 +1,100 @@
+/* ==================================================================================
+ * AUTHOR       : sikula
+ * VERSION      : 1.0.5
+ * LAST UPDATE  : Tue Oct 21
+ * DESCRIPTION  : A javascript program using CasperJS to save a map, from
+ *                OpenStreet Maps as an image. The program can easily manipulate the
+ *                map to create custom map-images.
+ *
+ * NOTES        : Keep the viewport size below 2000 x 2000 otherwise it takes a long
+ *                time to get the picture.
+ * ==================================================================================
+ */
+
+/* Create a new CasperJS instance and specify the appropriate settings */
+var casper  = require('casper').create({
+    pageSettings: {
+        userAgent: 'Mozilla/5.0 (Windows; U; Windows NT 5.1; pt-BR) AppleWebKit/533.3 (KHTML, like Gecko)  QtWeb Internet Browser/3.7 http://www.QtWeb.net'
+    },
+    viewportSize: {
+        width:  1500,
+        height: 1000,
+    },
+    retryTimeout: 20,
+});
+
+/* Creates an anonymous function which will run automatically, used for setting variables in this case */
+(function() {
+    base    = "http://www.openstreetmap.org/#map=";
+    zoom    = casper.cli.get("zoom") || 17;
+    lat     = casper.cli.get("lat")  || 42.35905;
+    lon     = casper.cli.get("lon")  || -71.05726;
+    layer   = "&layers=T";
+    webpage = base + zoom + "/" + lat + "/" + lon + layer;
+})();
+
+
+var Mapper  = (function() {
+
+    /* Constructor */
+    function Mapper() {
+        /* Instance Variables */
+        this.city   = casper.cli.get("city");
+    };
+
+    // public:
+        /* Get the city name from the command line argument */
+        Mapper.prototype.get_city       = function() {
+            return this.city;
+        };
+
+        /* Retrieve the latitude of a city from  http://dbsgeo.com/latlon/ */
+        Mapper.prototype.get_lat_lon    = function(city) {
+
+        };
+
+        /* Return the latitude only */
+        Mapper.prototype.get_lat        = function() {
+
+        };
+
+        /* Return the longitude only */
+        Mapper.prototype.get_long       = function() {
+
+        };
+
+    return Mapper;
+
+})();
+
+/* We begin by injecting ZeptoJS into our current DOM context */
+casper.start(webpage, function() {
+    casper.page.injectJs('resources/zepto.min.js');
+});
+
+
+/* Next we remove the side controls from the container so we don't crowd our viewport with useless elements */
+casper.then(function() {
+    this.evaluate(function() {
+        $('.leaflet-control-container').remove();
+    });
+});
+
+
+/*  Next we remove the sidebar */
+casper.then(function() {
+    this.evaluate(function() {
+        $('#sidebar').remove();
+    });
+});
+
+
+/* Finally we take a snapshot of <div id="map">...</div> */
+casper.then(function() {
+    this.captureSelector("test_map.png", "div#map");
+});
+
+
+casper.run(function() {
+    this.exit();
+});
---
+++ reddit_scrape.js
@@ -0,0 +1,149 @@
+/*
+ * ==================================================================================
+ * AUTHOR       : sikula
+ * VERSION      : 1.0.5
+ * LAST UPDATE  : Mon Oct 20
+ * DESCRIPTION  : A javascript program using CasperJS to extract the
+ *                title, url, and time posted of the posts from a specified
+ *                subreddit page.
+ * ==================================================================================
+ */
+
+// Required Libraries
+var casper      = require('casper').create({
+    pageSettings: {
+        loadImages:         false,
+        loadPlugins:        false,
+        userAgent:          'Mozilla/5.0 (Windows; U; Windows NT 5.1; pt-BR) AppleWebKit/533.3 (KHTML, like Gecko)  QtWeb Internet Browser/3.7 http://www.QtWeb.net'
+    },
+    retryTimeout:   20,
+});
+var colorizer   = require('colorizer').create('Colorizer');
+
+var webpage;
+// Variables
+var titles  = [];
+var links   = [];
+var times   = [];
+var posts   = [];
+
+
+(function() {
+    var subred  = casper.cli.get("subred");
+    if( !casper.cli.has("subred") {
+        webpage = "http://www.reddit.com/";
+    } else {
+        webpage = "http://www.reddit.com/r/" + subred;
+    };
+})();
+
+//----------------------------------------------------------
+// Class-like object for formatting output
+//----------------------------------------------------------
+var Formatter   = (function() {
+    /* Constructor */
+    function Formatter() {
+        /* Instance Variables */
+    };
+
+    // public:
+        /* Format for titles */
+        Formatter.prototype.title   = function(text) {
+            return colorizer.format(text, {
+                fg:     'green',
+                bold:   false,
+            });
+        };
+
+        /* Format for links */
+        Formatter.prototype.links   = function(text) {
+            return colorizer.format(text, {
+                fg:     'magenta',
+                bold:   false,
+            });
+        };
+
+    return Formatter;
+
+})();
+
+
+//----------------------------------------------------------
+// Class-like objectt for extracting data from webpage
+//----------------------------------------------------------
+var Extractor = (function() {
+
+    /* Constructor */
+    function Extractor() {
+        /* Instance Variables */
+    };
+
+    // private:
+        /* Take as input two arrays and returns a single array of array_1[i] array_2[i] */
+        function zip(arrays) {
+            return arrays[0].map(function(_, i) {
+                return arrays.map(function(array) {
+                    return array[i];
+                });
+            });
+        };
+
+
+    // public:
+        /* Returns an array of the titles scraped by Casper */
+        Extractor.prototype.get_titles    = function() {
+            var titles  = document.querySelectorAll('p.title a');
+            return Array.prototype.map.call(titles, function(element) {
+                return element.textContent;
+            });
+        };
+
+
+        /* Returns an array of links scraped by Casper */
+        Extractor.prototype.get_links       = function() {
+            var links   = document.querySelectorAll('p.title a');
+            return Array.prototype.map.call(links, function(element) {
+                return element.getAttribute('href');
+            });
+        };
+
+
+        /* Returns an array containing the time the post was posted */
+        Extractor.prototype.get_times       = function() {
+            var times   = document.querySelectorAll('p.tagline time.live-timestamp');
+            return Array.prototype.map.call(times, function(element) {
+                return element.getAttribute('title');
+            });
+        };
+
+
+        /* Returns the reddit post, which is title + link */
+        Extractor.prototype.posts           = function(titles, links) {
+            return zip([titles, links]);
+        };
+
+
+    return Extractor;
+
+
+})();
+
+
+
+casper.start(webpage, function() {
+    var extractor   = new Extractor();
+    links   = links.concat(this.evaluate(extractor.get_links));
+    titles  = titles.concat(this.evaluate(extractor.get_titles));
+    times   = times.concat(this.evaluate(extractor.get_times));
+    posts   = posts.concat(extractor.posts(titles, links));
+});
+
+
+casper.run(function() {
+    var format  = new Formatter();
+    for( i = 1 ; i <= 10; i++ ) {
+        this.echo(format.title("[ " + i + " ] " + posts[i][0] + " : " + times[i]));
+        this.echo(format.links("          "     + posts[i][1]));
+    };
+    this.exit();
+});
